8982fd3b77e5d304cff89b38fba3c4485352d0d6
Kagumi-Katai <vinicius_apple22@outlook.com>
2024-09-27 20:30:21 -0300

    subindo o index e os pacotes

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express") //para poder usar o express
+const server = express() //para criar o servidor
+
+//pegando as informações do servidor na porta padrão
+server.get("/", (req, ans)=> {
+    return ans.json({mensagem: "Hello NODE!"})
+})
+
+server.get("/porta1", (req, ans)=> {
+    return ans.json({mensagem: "Porta 1"})
+})
+
+server.get("/porta2", (req, ans)=> {
+    return ans.json({mensagem: "Porta 2"})
+})
+
+//escutando o server no endereço 3300
+server.listen(3300, ()=> {
+    console.log("Server OK")
+})
