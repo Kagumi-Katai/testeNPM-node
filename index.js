8c160855e9d3e86adf47bfebb1f3062dc0ef0e44
Kagumi-Katai <vinicius_apple22@outlook.com>
2024-09-27 21:42:14 -0300

    mocando o bd em /objetos

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express") //para poder usar o express
 const server = express() //para criar o servidor
+const alunos = require("./src/objetos.json") //busca uma lista de objetos no BD (nesse caso o BD é mocado)
 
 //pegando as informações do servidor na porta padrão
 server.get("/", (req, ans)=> {
@@ -11,12 +12,13 @@ server.get("/porta1", (req, ans)=> {
 })
 
 server.get("/porta2", (req, ans)=> {
-    return ans.json({mensagem: "Porta 2"})
-})
-
-server.get("/porta3", (req, ans)=> {
     return ans.json({mensagem: "testando o nodemon!!"})
 })
+
+server.get("/objetos", (req, ans)=> {
+    return ans.json(alunos)
+})
+
 
 //escutando o server no endereço 3300
 server.listen(3300, ()=> {
